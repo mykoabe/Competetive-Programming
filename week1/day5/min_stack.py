e6ae3c2bcc8cfe380c629dc8168b34028e817208
Mekuanint Abebe <se.mekuanint.abebe@gmail.com>
2021-12-18 15:23:43 +0300

    min stack

---
+++ week1/day5/min_stack.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        minimum = self.stack[0]
+        for i in self.stack:
+            if i < minimum:
+                minimum = i
+        return minimum
