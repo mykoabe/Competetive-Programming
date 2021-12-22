c81654a996be8f46ff5287172750ff58d6e6e493
Mekuanint Abebe <se.mekuanint.abebe@gmail.com>
2021-12-22 15:15:13 +0300

    day6

---
+++ week1/day6/shortest_sub_array.py
@@ -0,0 +1,23 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def shortestSubarray(self, A: List[int], K: int) -> int:
+        pre = [0]
+        for num in A:
+            pre.append(pre[-1]+num)
+
+        deque = deque()
+        result = float(inf)
+        for i, sum_ in enumerate(pre):
+
+            while(deque and deque[-1][1] >= sum_):
+                deque.pop()
+
+            while deque and sum_ - deque[0][1] >= K:
+                result = min(i-deque[0][0], result)
+                deque.popleft()
+
+            deque.append([i, sum_])
+        return result if result != float(inf) else -1
