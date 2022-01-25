f6c3950f82dc9c814631ee789454b0c80311f0aa
Mekuanint Abebe <57587084+mykoabe@users.noreply.github.com>
2022-01-25 21:24:46 +0200

    Time: 72 ms (68.95%), Space: 14.5 MB (11.50%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,43 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        r = l1
-        p1 = l1
-        p2 = l2
-        c = 0
-        q = l2
-        # b1 = l1
-        # b2 = l2
-        while p1 != None and p2 != None:
-            s = 0
-            s += p1.val + p2.val + c
-            c = s // 10
-            s = s % 10
-            p1.val = s
-            b1 = p1
-            b2 = p2
-            p1 = p1.next
-            p2 = p2.next
-        while p1 != None:
-            s = 0
-            s += p1.val + c
-            c = s // 10
-            s = s % 10
-            b1 = p1
-            p1.val = s
-            p1 = p1.next
-        while p2 != None:
-            s = 0
-            s += p2.val + c
-            c = s // 10
-            s = s % 10
-            p2.val = s
-            b1.next = p2
-            b2 = p2
-            b1 = b1.next
-            p2 = p2.next
-        if c != 0:
-            new = ListNode(c)
-            b1.next = new
-        return r
-        
+        s1 = []
+        s2 = []
+        while l1 or l2:
+            if l1:
+                s1.insert(0,str(l1.val))
+                l1 = l1.next
+            if l2:
+                s2.insert(0,str(l2.val))
+                l2 = l2.next
+        total = int(''.join(s1)) + int(''.join(s2))
+        sumlist = list(map(int, str(total)))
+        head = ListNode(0)
+        res = head
+        N = len(sumlist)
+        for i in range(N):
+            curr = ListNode(sumlist[N-i-1])
+            head.next = curr
+            head = curr
+        return res.next
