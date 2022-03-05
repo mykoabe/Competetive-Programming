f8ddd111161adc110c3276ad0dc5b6a784a3b234
Mekuanint Abebe <57587084+mykoabe@users.noreply.github.com>
2022-03-05 19:13:03 +0200

    Time: 114 ms (43.92%), Space: 13.9 MB (99.55%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,19 +1,29 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-       
-
-        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-
-        def dfs(image, r, c, newColor, color):
-            if  (0 <= r < len(image) and \
-                 0 <= c < len(image[0]) and \
-                    image[r][c] == color):
+        
+        directions = [(0,-1), (0,1), (-1,0), (1,0)]
+        
+        m = len(image)
+        n = len(image[0])
+        
+        def dfs(image, r, c, newColor, oldColor):
+            #old color is the color at image[sr][sc]
+            
+            if (0 <= r < len(image) ) and (0 <= c < len(image[0])) and image[r][c] == oldColor:
+                
                 for d in directions:
-                    dfs(image, r+d[0], c+d[1], newColor, color)
+                    dfs(image, r+d[0], c+d[1], newColor, oldColor)
                     image[r][c] = newColor
             else:
                 return 
-        color = image[sr][sc]
-        if color == newColor: return image
-        dfs(image, sr, sc, newColor, color)
+            
+        oldColor = image[sr][sc]
+        if oldColor == newColor: return image 
+        dfs(image, sr, sc, newColor, oldColor)
         return image
+        
+            
+                
+                
+            
+        
