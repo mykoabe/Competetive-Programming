532e80fcb84802f2e8bf43108599fe0ab7957288
Mekuanint Abebe <57587084+mykoabe@users.noreply.github.com>
2022-05-06 11:18:18 -0700

    Update Longest-word.py

--- Binary Tree/Longest-word.py
+++ Binary Tree/Longest-word.py
@@ -1,12 +1,47 @@
- #my brute method before tring trie implemetation
-        if not words:
-            return []
-        words.sort()
-        res = ['']
-        longest = ''
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.isEnd = False
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        root = TrieNode()
+        
+        #insert every word letter by letter to the trie
         for word in words:
-            if word[:-1] in res:
-                if len(word) > len(longest):
-                    longest = word
-                res.append(word)
-        return longest
+            curr = root
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.isEnd = True
+        
+        res = ''
+        for word in words:            
+            if len(word) < len(res):
+                continue         
+            cur = root
+            for letter in word:
+                cur = cur.children[letter]
+                if not cur.isEnd:
+                    break           
+            if cur.isEnd and (len(word) > len(res) or (len(word) == len(res) and word < res)):
+                res = word        
+            
+        return res
+            
+        #my brute method before tring trie implemetation
+        # if not words:
+        #     return []
+        # words.sort()
+        # res = ['']
+        # longest = ''
+        # for word in words:
+        #     if word[:-1] in res:
+        #         if len(word) > len(longest):
+        #             longest = word
+        #         res.append(word)
+        # return longest
+        
+        
+                
+                
