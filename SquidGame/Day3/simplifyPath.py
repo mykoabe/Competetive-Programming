7414a7d1c2d9e61a55a300e4dbe19f7bb6c63448
Mekuanint Abebe <se.mekuanint.abebe@gmail.com>
2023-02-03 18:37:01 +0300

    day3

--- SquidGame/Day3/simplifyPath.py
+++ SquidGame/Day3/simplifyPath.py
@@ -1,16 +1,29 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        stack = []
+        for p in path.split('/'):
+            if p == '..':
+                if stack:
+                    stack.pop()
+            elif p and p != '.':
+                stack.append(p)
+        return '/' + '/'.join(stack)
+        #
+        
+
+
+
         #let s_str is equal to separated string
-        s_str = path.split('/')
-        print(s_str)
-        result = []
-        n = len(s_str)
-        for i in range(n):
-            if s_str[i] and s_str[i] != '.' and s_str[i] != '..':
-                result.append(s_str[i])
-            if s_str[i] == '..':
-                if result:
-                    result.pop() 
-        #print(s_str)
-        return '/' + '/'.join(result)
+        # s_str = path.split('/')
+        # print(s_str)
+        # result = []
+        # n = len(s_str)
+        # for i in range(n):
+        #     if s_str[i] and s_str[i] != '.' and s_str[i] != '..':
+        #         result.append(s_str[i])
+        #     if s_str[i] == '..':
+        #         if result:
+        #             result.pop() 
+        # #print(s_str)
+        # return '/' + '/'.join(result)
         
--- SquidGame/Day3/validPalindrome.py
+++ SquidGame/Day3/validPalindrome.py
@@ -1,10 +1,21 @@
+# another solution
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+        def isPalindrome(s):
+            return s == s[::-1]
         left, right = 0, len(s) - 1
         while left < right:
             if s[left] != s[right]:
-                leftSkip, rightSkip = s[left+1:right+1], s[left:right]
-                return (leftSkip == leftSkip[::-1] or rightSkip == rightSkip[::-1])
+                return isPalindrome(s[left:right]) or isPalindrome(s[left+1:right+1])
             left, right = left + 1, right - 1
         return True
-        
+    
+    
+        # left, right = 0, len(s) - 1
+        # while left < right:
+        #     if s[left] != s[right]:
+        #         leftSkip, rightSkip = s[left+1:right+1], s[left:right]
+        #         return (leftSkip == leftSkip[::-1] or rightSkip == rightSkip[::-1])
+        #     left, right = left + 1, right - 1
+        # return True
+# another solution
